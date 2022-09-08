09ab0afca03eeab320b6fabc12f3781c16359c6a
grzesiekpwrwroc <grzegorz.jarus@gmail.com>
2022-09-09 01:41:02 +0200

    Add method to transfer List<Point> to double[][] array

--- src/main/java/pl/coderslab/leaflets/util/Arrays.java
+++ src/main/java/pl/coderslab/leaflets/util/Arrays.java
@@ -7,20 +7,30 @@ import java.util.LinkedList;
 import java.util.List;
 
 public class Arrays {
-    public static List<Point> transferToList(double[][] array){
+    public static List<Point> transferToList(double[][] array) {
         List<Point> points = new LinkedList<>();
         for (int i = 0; i < array.length; i++) {
-            Point point= new Point();
+            Point point = new Point();
             point.setLatitude(array[i][0]);
             point.setLongitude(array[i][1]);
             points.add(point);
-        }return points;
+        }
+        return points;
     }
 
-    public static void printArray(double[][] array){
+    public static void printArray(double[][] array) {
         for (int i = 0; i < array.length; i++) {
-            System.out.print(array[i][0]+ "  ");
+            System.out.print(array[i][0] + "  ");
             System.out.println(array[i][1]);
         }
+    }
+
+    public static double[][] transferToArray(List<Point> points) {
+        double[][] pointsArrayFromList = new double[points.size()][2];
+        for (int i = 0; i < points.size(); i++) {
+            pointsArrayFromList[i][0] = points.get(i).getLatitude();
+            pointsArrayFromList[i][1] = points.get(i).getLongitude();
+        }
+        return pointsArrayFromList;
     }
 }
